dcf8bf9e892ed538cde2773728091856a1938775
Wojciech Szydlak <szydwoj@gmail.com>
2019-08-24 16:48:58 +0200

    Updates

---
+++ precache-manifest.cb0b55e914a393ac477d5c3ca97ad190.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a26ce225e19515c0b04964a9d7c54e3",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "cbe4e01da2434da16707",
+    "url": "/movie-search/static/css/main.fb9ed214.chunk.css"
+  },
+  {
+    "revision": "99f2b019b7c385010794",
+    "url": "/movie-search/static/js/2.f8b8e6c4.chunk.js"
+  },
+  {
+    "revision": "cbe4e01da2434da16707",
+    "url": "/movie-search/static/js/main.6b940013.chunk.js"
+  },
+  {
+    "revision": "467f242a197283b43b88",
+    "url": "/movie-search/static/js/runtime~main.5125b6b9.js"
+  }
+]);
